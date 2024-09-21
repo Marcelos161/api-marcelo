d73698a743552172a8c1463e65e2d0192935699b
Marcelo Diogo <marcelos.diogos@outlook.com>
2024-09-21 14:53:02 -0300

    second commit

--- functions/cloudinary.js
+++ functions/cloudinary.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  const cloudName = 'SEU_CLOUD_NAME';
-  const apiKey = 'SUA_API_KEY';
-  const apiSecret = 'SEU_API_SECRET';
+  const cloudName = 'dzgolou3t';
+  const apiKey = '346449748872511';
+  const apiSecret = 'qehHw3EC7Mh1HimscOKq1_BqAfM';
 
   const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image`;
 
